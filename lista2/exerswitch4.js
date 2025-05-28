3ae71844ff09512c4d245eed36888ca72176e3ba
devvMiguel <miguelgalvao_galvao@hotmail.com>
2025-05-28 12:20:00 -0300

    pasta trilha zero

--- lista2/exerswitch4.js
+++ lista2/exerswitch4.js
@@ -4,33 +4,36 @@ let codOperacao, saldo = 1000.00, novoSaldo, valorSaque,operacao, deposito;
 
 codOperacao = leia.questionInt("Operacao: ")
 
-switch(codOperacao){
-    case 1:
-        operacao = "Saldo";
-        console.log(`\nOperação - ${operacao}`);
-        console.log(`\nSaldo: R$${saldo.toFixed(2)}`);
-        break;
-        
-    case 2: 
-        operacao = "Saque";
-        console.log(`\nOperação - ${operacao}`);
-        switch(true){
-            case (valorSaque > saldo): 
-                console.log("Saldo Insuficiente!");
-                break;  
-            default:       
-                novoSaldo = saldo - valorSaque;
-                console.log(`\nNovo Saldo: R$ ${novoSaldo.toFixed(2)}`);
-        }
-        break;
-    case 3:
-        operacao = "Depósito";
-        console.log(`\nOperação - ${operacao}`);
-        deposito = leia.questionFloat("Valor: R$ ")
-        novoSaldo = saldo + deposito;
-        console.log(`\nNovo Saldo: R$ ${novoSaldo.toFixed(2)}`);
-        break;
 
-    default:
-        console.log("Operação Inválida!")
-}
+// AINDA TRABALHANDO NO CASE 2 rs
+
+// switch(codOperacao){
+//     case 1:
+//         operacao = "Saldo";
+//         console.log(`\nOperação - ${operacao}`);
+//         console.log(`\nSaldo: R$${saldo.toFixed(2)}`);
+//         break;
+        
+//     case 2: 
+//         operacao = "Saque";
+//         console.log(`\nOperação - ${operacao}`);
+//         switch(true){
+//             case (valorSaque > saldo): 
+//                 console.log("Saldo Insuficiente!");
+//                 break;  
+//             default:       
+//                 novoSaldo = saldo - valorSaque;
+//                 console.log(`\nNovo Saldo: R$ ${novoSaldo.toFixed(2)}`);
+//         }
+//         break;
+//     case 3:
+//         operacao = "Depósito";
+//         console.log(`\nOperação - ${operacao}`);
+//         deposito = leia.questionFloat("Valor: R$ ")
+//         novoSaldo = saldo + deposito;
+//         console.log(`\nNovo Saldo: R$ ${novoSaldo.toFixed(2)}`);
+//         break;
+
+//     default:
+//         console.log("Operação Inválida!")
+// }
